acbf2956ccb16d86e37e19c2e760ea893b8f13e1
xiaoyue26 <296671657@qq.com>
2017-11-22 11:30:58 +0800

    first commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,14 @@
+/**
+ * Created by xiaoyue26 on 17/11/22.
+ */
+public class Demo {
+    public void test1() {
+        System.out.println("here");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.test1();
+        System.out.println("there");
+    }
+}
