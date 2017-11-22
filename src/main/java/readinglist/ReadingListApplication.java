51d9d624fb687eb8097ffd61c088a73ad04a29ad
xiaoyue26 <296671657@qq.com>
2017-11-22 15:51:30 +0800

    ch03 add login

--- src/main/java/readinglist/ReadingListApplication.java
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -1,7 +1,10 @@
 package readinglist;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,10 +14,16 @@ public class ReadingListApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(ReadingListApplication.class, args);
     }
-    
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addRedirectViewController("/", "/readingList");
+        registry.addViewController("/login").setViewName("login");
     }
-    
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ReaderHandlerMethodArgumentResolver());
+    }
+
 }
