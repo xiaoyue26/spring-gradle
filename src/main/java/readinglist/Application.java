8ef63a82cc4a0e675018b57902fa382f834a7f5d
xiaoyue26 <296671657@qq.com>
2017-11-23 14:48:31 +0800

    gradle build executable jar and deployable war

---
+++ src/main/java/readinglist/Application.java
@@ -0,0 +1,24 @@
+package readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by xiaoyue26 on 17/11/23.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
--- src/main/java/readinglist/ReadingListApplication.java
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -2,6 +2,8 @@ package readinglist;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -11,13 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class ReadingListApplication extends WebMvcConfigurerAdapter {
 
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     public static void main(String[] args) {
         SpringApplication.run(ReadingListApplication.class, args);
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+
         registry.addViewController("/login").setViewName("login");
+        logger.info("add login controller ok ");
     }
 
     @Override
