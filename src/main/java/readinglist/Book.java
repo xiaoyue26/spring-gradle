0e5d2b3c5fbd960bb1a2afc986573d8304992aa2
xiaoyue26 <296671657@qq.com>
2017-11-26 21:19:11 +0800

    add primary mysql config

--- src/main/java/readinglist/Book.java
+++ src/main/java/readinglist/Book.java
@@ -1,12 +1,10 @@
 package readinglist;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "book")
 public class Book {
 
 	@Id
--- src/main/java/readinglist/Reader.java
+++ src/main/java/readinglist/Reader.java
@@ -5,69 +5,71 @@ import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
+@Table(name = "Reader")
 public class Reader implements UserDetails {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  @Id
-  private String username;
-  
-  private String fullname;
-  private String password;
-  
-  public String getUsername() {
-    return username;
-  }
-  
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  
-  public String getFullname() {
-    return fullname;
-  }
-  
-  public void setFullname(String fullname) {
-    this.fullname = fullname;
-  }
-  
-  public String getPassword() {
-    return password;
-  }
-  
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    @Id
+    private String username;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Arrays.asList(new SimpleGrantedAuthority("ROLE_READER"));
-  }
+    private String fullname;
+    private String password;
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    public String getFullname() {
+        return fullname;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_READER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
