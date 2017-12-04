8ca5949cafef3c951fc3f14028f0c99844690a2f
xiaoyue26 <296671657@qq.com>
2017-12-04 17:04:55 +0800

    ADD serial configure

--- src/main/java/com/xiaoyue/nov/practice/redis/RedisConfigure.java
+++ src/main/java/com/xiaoyue/nov/practice/redis/RedisConfigure.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by xiaoyue26 on 17/12/4.
@@ -27,6 +29,8 @@ public class RedisConfigure {
     public RedisTemplate<String, Product> redisTemplate(RedisConnectionFactory cf) {
         RedisTemplate<String, Product> redis = new RedisTemplate<>();
         redis.setConnectionFactory(cf);
+        /*redis.setStringSerializer(new StringRedisSerializer());// 配置非默认的序列化类
+        redis.setValueSerializer(new Jackson2JsonRedisSerializer<Product>(Product.class));*/
         return redis;
     }
 }
