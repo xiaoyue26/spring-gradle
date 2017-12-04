c2540c33b6cb08b10a07cb38e56b585d997477cc
xiaoyue26 <296671657@qq.com>
2017-12-04 15:23:24 +0800

    add query by annotion

--- src/main/java/com/xiaoyue/nov/storage/ReaderRepository.java
+++ src/main/java/com/xiaoyue/nov/storage/ReaderRepository.java
@@ -1,6 +1,12 @@
 package com.xiaoyue.nov.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReaderRepository extends JpaRepository<Reader, String> {
+
+    @Query("select username,password,fullname from Reader")
+    List<Reader> findAllGmailReaders();
 }
